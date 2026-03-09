1edfd9f4a0a89c0873c5396bf11acfe7ff79a73a
nyasimimalian <nyasimimalian@users.noreply.github.com>
2026-03-09 14:59:05 +0300

    updating package.json

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,9 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    host: "localhost",
+    port: 3000,
+    strictPort: true,
     hmr: {
       overlay: false,
     },
